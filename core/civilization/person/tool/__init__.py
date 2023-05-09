ab8dba8710280f99ac242bc1982843022af29851
Taeho <adldotori@gmail.com>
2023-05-09 19:10:33 +0900

    Feature/browser (#24)
    * feat: browser test
    
    * feat: update browser tool
    
    * feat: update poetry lock file
    
    * feat: backmerge
    
    * feat: resolve comment

--- core/civilization/person/tool/__init__.py
+++ core/civilization/person/tool/__init__.py
@@ -1,5 +1,7 @@
 from typing import Dict
 
+from core.civilization.person.tool.browser import Browser
+
 from .base import BaseTool, BuildParams, UseParams
 from .coded import CodedTool
 from .default import CodeWriter, Terminal
@@ -25,6 +27,18 @@ default_tools: Dict[str, BaseTool] = {
             "Instruction should be a path to a file. "
             "Extra should be a valid code. "
             "Output will be the 'success' or 'error'. "
+        ),
+    ),
+    "browser": Browser(
+        "browser",
+        (
+            "Surfing the web on a browser. "
+            "Instruction should be one valid command. (ex. open, scroll, move, click, write, close) "
+            "Extra should be a valid input for that command. "
+            # "You can also enter the key directly through the write command, such as Keys.RETURN and Keys.SHIFT. " # TODO
+            "open: <url>, scroll: <position>, move: <css selector>, click: <css selector>, write: <{css selector: input}>, close: <empty> "
+            'ex. open: https://www.google.com, scroll: 0,0, move: div#iur > div[1] > div > h3[1], click: div#jZ2SBf > div[1] > span > b, write: {"textarea#APjFqb": "hello"}, close: '
+            "Output will be the page's contents. "
         ),
     ),
 }
--- core/config/env.py
+++ core/config/env.py
@@ -28,6 +28,6 @@ settings: DotEnv = {
     "PORT_START": int(os.getenv("PORT_START", "50000")),
     "PORT_RANGE": int(os.getenv("PORT_RANGE", "10")),
     "HOST": os.getenv("HOST", "127.0.0.1"),
-    "REDIS_HOST": os.getenv("REDIS_HOST", "localhost"),
+    "REDIS_HOST": os.getenv("REDIS_HOST", ""),
     "REDIS_PORT": os.getenv("REDIS_PORT", "6379"),
 }
